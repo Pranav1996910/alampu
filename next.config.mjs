f5be3566be4d9486338982c85b16cd45a98dbec0
PranavJ96 <pranavjayanayak@gmail.com>
2025-03-18 22:34:07 +0100

    update configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  productionBrowserSourceMaps: true, // Optional: for debugging
+};
 
 export default nextConfig;
